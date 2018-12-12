2ac3454eb341a9bb7e30ea74e23a366c60006817
wanghuan <xdhuxc@163.com>
2018-12-12 22:17:46 +0800

    编写所需数据库脚本。

--- sql/chitchat.sql
+++ sql/chitchat.sql
@@ -3,7 +3,14 @@ create database chitchat if not exists;
 use chitchat;
 
 create table if not exits `users`(
-  id int(11) auto_increment,
-  uuid varchar(64) not null unique,
-  name varchar(100) not null,
-)
+  `id` int(11) AUTO_INCREMENT,
+  `uuid` varchar(64) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY(`id`),
+  UNIQUE KEY `name` (`name`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
